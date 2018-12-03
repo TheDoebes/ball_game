3a84533d89c4900caa416f8fa0ed0282ff2851e2
TheDoebes <TheDoebes@users.noreply.github.com>
2018-12-03 16:31:01 -0500

    Update game.vhd

--- game.vhd
+++ game.vhd
@@ -2,28 +2,68 @@
 
 entity game is
 	generic (
-		paddlex
-		paddley
-				);
+		paddle_width	: integer := 10;
+		paddle_height	: integer := 120;
+		ball_size		: integer := 20;
+		h_pixels			: integer := 800;
+		v_pixels			: integer := 600
+				);									--Note that the last lines^^vv MUST omit the semicolon
 	port (
 		ball_xpos	:	out	integer;
 		ball_ypos	:	out	integer;
 			);
-end game;
+END game;
 
 architecture behavior of game is
 --signals
 
+	--Coordingates of the top-left coordinate (read, smallest) of the square ball.
 	signal ballx : integer := 0;
 	signal bally : integer := 0;
-	signal paddlel : integer := 0;
-	signal paddelr : integer := 0;
 	
+	--Current vertical coordinates of the tops of the left and right paddles.
+	signal paddlel : integer := 0;
+	signal paddler : integer := 0;
+	
+	
+	--Player scores
+	signal score1	: integer :=0;
+	signal score2	: integer :=0;
+	
+	--Game state machine
+	signal reset		: std_logic :='1';
+	signal ball_reset : std_logic :='1';
 --constants
 	--signal paddleSize : integer := 120;
 
-begin
+BEGIN
 
---start with score logic
+--reset logic, trigger on game start and at the end of win seqeunce.
+	reset_logic:process(reset, ball_reset)
+	BEGIN
+	END; --reset_logic
+	
+	
 
-score:process(ballx, bally)
+--scoring logic
+
+	score:process(ballx, bally)
+	BEGIN
+		if(ballx = 0) then 
+		--player 2 scores
+			score2 = score2 + 1;
+			ball_reset = '1';
+		elsif(ballx = h_pixels) then 
+		--player 1 scores
+			score1 = score1 + 1;
+			ball_reset = '1';
+		else 
+		--ball is in play
+		END if;
+		
+		--check for win conditions
+	END; --score
+		
+		
+		
+			
