eb75ff573ce11877149d54e05fc0e0f50b5915aa
TheDoebes <TheDoebes@users.noreply.github.com>
2018-12-03 16:09:07 -0500

    Update game.vhd

--- game.vhd
+++ game.vhd
@@ -1,11 +1,26 @@
 
 
+entity game is
+	generic (
+		paddlex
+		paddley
+				);
+	port (
+		ball_xpos	:	out	integer;
+		ball_ypos	:	out	integer;
+			);
+end game;
+
 architecture behavior of game is
 --signals
 
 	signal ballx : integer := 0;
 	signal bally : integer := 0;
-	signal 
+	signal paddlel : integer := 0;
+	signal paddelr : integer := 0;
+	
+--constants
+	--signal paddleSize : integer := 120;
 
 begin
 
