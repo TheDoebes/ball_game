906596c335a0d73d38b4d5e5ee208dc4ef0f20fc
TheDoebes <TheDoebes@users.noreply.github.com>
2018-12-04 13:54:26 -0500

    Implemented collision logic
    also fixed assignment syntax errors

--- game.vhd
+++ game.vhd
@@ -1,4 +1,6 @@
-
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
 
 entity game is
 --a generic is a sepcial constant. Think of it as a parameter that is only optionally specified. 
@@ -8,7 +10,8 @@ entity game is
 		paddle_height	: integer := 120;
 		ball_size		: integer := 20;
 		h_pixels			: integer := 800;
-		v_pixels			: integer := 600
+		v_pixels			: integer := 600;
+		max_score		: integer := 9;
 				);									--Note that the last lines^^vv MUST omit the semicolon
 	port (
 		ball_xpos	:	out	integer;
@@ -19,14 +22,15 @@ END game;
 architecture behavior of game is
 --signals
 
-	--Coordingates of the top-left coordinate (read, smallest) of the square ball.
+	--Coordingates of the top-left coordinate (read, smallest) of the square ball, and its direction.
 	signal ballx : integer := h_pixels/2;
 	signal bally : integer := v_pixels/2;
+	signal movex : std_logic := '0';
+	signal movey : std_logic := '0';
 	
 	--Current vertical coordinates of the tops of the left and right paddles.
-	signal paddlel : integer := (v_pixels + paddle_height)/2;
-	signal paddler : integer := (v_pixels + paddle_height)/2;
-	
+	signal paddle_L : integer := (v_pixels + paddle_height)/2;
+	signal paddle_R : integer := (v_pixels + paddle_height)/2;
 	
 	--Player scores
 	signal score1	: integer :=0;
@@ -41,38 +45,38 @@ architecture behavior of game is
 BEGIN
 
 
-	mechanics:process(ballx, bally, reset, ball_reset)
+	mechanics:process(ballx, bally, movex, movey, reset, ball_reset)
 	BEGIN
+	
 	--Score Logic
 		if(ballx = 0) then 
 		--player 2 scores
-			score2 = score2 + 1;
-			ball_reset = '1';
+			score2 <= score2 + 1;
+			ball_reset <= '1';
 		elsif(ballx = h_pixels) then 
 		--player 1 scores
-			score1 = score1 + 1;
-			ball_reset = '1';
-		else 
-		--ball is in play
-		END if;
-		if(bally = ball_size) then
-		--reverse y component, to bounce off
-		elsif(bally = v_pixels) then
+			score1 <= score1 + 1;
+			ball_reset <= '1';
+		END if; --ball is in play
 		
-		END if;
+	--Win condition logic
+		if(score1 = max_score) then
+		--player 1 wins
 		
-	--check for win conditions
+		elsif(score2 = max_score) then
+		--player 2 wins
 		
+		END if; --then continue play normally
 		
 	--reset logic, trigger on game start and at the end of win seqeunce.
 		if(reset = '1') then
 		--reset the game state
-			ball_reset = '1';
-			score1 = 0;
-			score2 = 0;
-			paddlel = (v_pixels + paddle_height)/2;
-			paddler = (v_pixels + paddle_height)/2;
-			reset = '0';
+			ball_reset <= '1';
+			score1 <= 0;
+			score2 <= 0;
+			paddle_L <= (v_pixels + paddle_height)/2;
+			paddle_R <= (v_pixels + paddle_height)/2;
+			reset <= '0';
 		End if;
 		if(ball_reset = '1') then
 		--the ball has scored, and should be moved to the starting location
@@ -80,6 +84,29 @@ BEGIN
 			bally <= v_pixels/2;
 			ball_reset <= '0';
 		END if;
+		
+	--Collision logic
+	
+		if(ballx = 0 + paddle_width AND bally - ball_size < paddle_L AND bally > paddle_L - paddle_height) then --left paddle hits
+		--then it should start moving in the x-direction
+			movex <= '1';
+			
+		elsif(ballx + ball_size = h_pixels - paddle_width AND bally - ball_size < paddle_R AND bally > paddle_R - paddle_height) then --right paddle hits
+		--and vice versa
+			movex <= '0';
+		END if;
+		if(bally - ball_size = 0) then --see if hitting top or bottom
+		--move with y axis
+			movey <= '1';
+		elsif(bally = v_pixels) then
+		--move against y axis
+			movey <= '0';
+		END if; 
+		
+	--Movement logic
+		
+		
+		
 	END; --mechanics
 		
 		
--- hw_image_generator.vhd
+++ hw_image_generator.vhd
@@ -22,6 +22,7 @@
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
 
 ENTITY hw_image_generator IS
 	GENERIC(
